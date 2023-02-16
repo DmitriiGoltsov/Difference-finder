556a8ef0b28a3fee218e8a6cb38175ba60566243
Dmitrii Goltsov <dmitriy.goltsov@mail.ru>
2023-02-16 21:35:31 +0100

    App class now can take two parameters (file pathes) and contains picocli code that shows information about the programme.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,49 +5,49 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.SortedMap;
-
+import java.util.concurrent.Callable;
 
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App {
+public final class App implements Callable<Integer> {
 
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format")
-    private String format;
+    private String formatName;
 
-    @Parameters(description = "A path to the 1st file.", paramLabel = "filepath1")
-    private static String filePath1;
+    @Parameters(paramLabel = "filepath1",
+            index = "0",
+            description = "path to first file")
+    private String filePath1;
 
-    @Parameters(description = "A path to the 2nd file.", paramLabel = "filepath2")
-    private static String filePath2;
+    @Parameters(paramLabel = "filepath2",
+            index = "1",
+            description = "path to second file")
+    private String filePath2;
 
     public static void main(String[] args) throws Exception {
 
-        Path path1 = Paths.get(filePath1).toAbsolutePath().normalize();
-        Path path2 = Paths.get(filePath2).toAbsolutePath().normalize();
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        Map<String, String> map1 = mapper.readValue(new File(String.valueOf(path1)), Map.class);
-        Map<String, String> map2 = mapper.readValue(new File(String.valueOf(path2)), Map.class);
-
-        System.out.println(map1);
-        System.out.println(map2);
+//        String result = Differ.generate("app/src/test/resources/file1.json", "app/src/test/resources/file2.json");
+//        System.out.println(result);
 
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
 
         System.out.println("Hello, world!");
-
     }
 
+    @Override
+    public Integer call() throws Exception {
+        // Обработку исключений пока ещё не проходили, потому в простейшем случае тут должен быть
+        // только вызов Differ.generate()
+        try {
+            String formattedDiff = Differ.generate(filePath1, filePath2, formatName);
+            System.out.println(formattedDiff);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return 1;
+        }
+
+        return 0;
+    }
 }
