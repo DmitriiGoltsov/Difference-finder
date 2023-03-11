dfc5b0b771af18d6f98fa42d3a7898ac5ca0eb2c
Dmitrii Goltsov <dmitriy.goltsov@mail.ru>
2023-03-11 18:49:27 +0100

    fix: corrected result_stylish.txt. Getters and setters in KeyStatus.java are now final. DifferTest.java is now final too.

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -9,7 +9,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class DifferTest {
+public final class DifferTest {
 
     private static String resultJson;
     private static String resultPlain;
